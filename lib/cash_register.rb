d3c1f12caa1bb4122737a44fe44e317f1a1c5574
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-24 20:51:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :employee_discount
 
-  def initialize(employee_discount)
+  def initialize
     @total = 0
     @employee_discount = employee_discount
   end
