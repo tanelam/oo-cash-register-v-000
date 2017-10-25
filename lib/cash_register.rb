a892141f8836e17efda01c1afbb468a7264b8e99
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-25 00:29:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title, price, quantity=1)
     self.total += price * quantity
-    @items << title
+    quantity.times {|x| @items << title }
   end
 
   def apply_discount
